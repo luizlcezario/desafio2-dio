751305c7559ffbacd376b8564e4c94c88aa2a241
luizlcezario <luizlcezario@gmail.com>
2023-02-24 17:47:36 -0300

    desafio2

---
+++ java_spring/desafio2/src/main/java/dio/desafio2/model/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package dio.desafio2.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * EnderecoRepository
+ */
+public interface EnderecoRepository extends CrudRepository<Endereco, String>{
+
+	
+}
